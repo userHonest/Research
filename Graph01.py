ea4dcfa7ab9b820cbaf0de9d490b488180d154d6
user_honest <92337570+userHonest@users.noreply.github.com>
2023-11-26 22:30:39 +0100

    Update Graph01.py

--- Graph01.py
+++ Graph01.py
@@ -1,1 +1,29 @@
-https://github.com/userHonest/Research/blob/main/Security%20Audit%20Report%20ArtQuest.pdf
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+lstCategories = ['HTTP Knowledge', 'HTTPS Programming', 'OWASP Awareness', 'Single Subject Course']
+iTotalStudents = 4
+lstValues = [4, 1, 0, 4]
+
+lstPercentages = [(iValue / iTotalStudents) * 100 for iValue in lstValues]
+
+fAveragePercentage = (3 / iTotalStudents) * 100
+lstAverageLine = [fAveragePercentage] * len(lstCategories)
+
+plt.figure(figsize=(10, 6))
+lstBars = plt.bar(lstCategories, lstPercentages, color='skyblue')
+plt.axhline(y=fAveragePercentage, color='r', linestyle='-', label='Average Trend Line (75%)')
+
+for bar in lstBars:
+    fYVal = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width()/2, fYVal, round(fYVal, 2), va='bottom', ha='center')
+
+plt.xlabel('Categories')
+plt.ylabel('Percentage of Students (%)')
+plt.title('Student Knowledge and Experience')
+plt.legend()
+
+plt.show()
+
